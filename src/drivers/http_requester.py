b2dcdc9c8056e8fc83c69c5bca1cf3a96771cde8
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-10-15 15:00:16 -0300

    test metodo "HttpRequester.request_from_page"

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -1,14 +1,15 @@
+from typing import Dict
 import requests
 
 
 class HttpRequester:
     __url: str
 
-    def __int__(self):
-        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+    #def __int__(self) -> None:
+    #    self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 
-    def request_from_page(self):
-        print(self.__url)
+    def request_from_page(self) -> Dict[str, str | int]:
+        self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 
         response = requests.get(self.__url)
 
