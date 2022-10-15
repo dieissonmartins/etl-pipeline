012fd7c02fa2596b90584e3f3baf453ad9ac4ce2
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-10-15 15:04:14 -0300

    add var context

--- src/drivers/http_requester_test.py
+++ src/drivers/http_requester_test.py
@@ -3,7 +3,8 @@ from .http_requester import HttpRequester
 
 def test_request_from_page(requests_mock):
     url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
-    requests_mock.get(url, status_code=200, text='alguma coisa')
+    response_context = 'request TRUE'
+    requests_mock.get(url, status_code=200, text=response_context)
 
     http_requester = HttpRequester()
     resquest_response = http_requester.request_from_page()
