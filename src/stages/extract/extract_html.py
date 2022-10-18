d85016c0f2c98b35cf7388c6048fc6eb7cd1ecfb
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-10-18 16:34:40 -0300

    tratativas com  contratos e erros

--- src/stages/extract/extract_html.py
+++ src/stages/extract/extract_html.py
@@ -1,7 +1,7 @@
-from src.drivers.http_requester import HttpRequester
-from src.drivers.html_collector import HtmlCollector
 from src.drivers.interfaces.http_collector import HtmlCollectorInterface
 from src.drivers.interfaces.http_requester import HttpRequesterInterface
+from src.stages.contracts.extract_contract import ExtractContract
+from datetime import date
 
 
 class ExtractHtml:
@@ -10,11 +10,14 @@ class ExtractHtml:
         self.__http_requester = init_http_requester
         self.__html_collector = init_html_collector
 
-    def extract(self):
+    def extract(self) -> ExtractContract:
         html_information = self.__http_requester.request_from_page()
 
         essential_information = self.__html_collector.collect_essential_information(html_information)
 
-        ret = essential_information
+        ret = ExtractContract(
+            raw_information_content=essential_information,
+            extraction_date=date.today()
+        )
 
         return ret
