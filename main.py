58435ac19bf46e1faa0fb001f61aadf372617fa0
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-10-19 10:41:33 -0300

    transforma dados

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from src.drivers.html_collector import HtmlCollector
 from src.drivers.http_requester import HttpRequester
 from src.stages.extract.extract_html import ExtractHtml
+from src.stages.transform.transform_raw_data import TransformRawData
 
 http_requester = HttpRequester()
 html_collector = HtmlCollector()
 
 # E – Extract
 extract_html = ExtractHtml(http_requester, html_collector)
-res = extract_html.extract()
+extract_html_data = extract_html.extract()
 
 # T – Transform
+transform_raw_data = TransformRawData()
+transform_html_data = transform_raw_data.transform(extract_html_data)
 
 # L – Load
---
+++ src/stages/transform/transform_raw_data.py
@@ -0,0 +1,77 @@
+from typing import List, Dict
+
+from src.stages.contracts.extract_contract import ExtractContract
+
+
+class TransformRawData:
+
+    def transform(self, extract_contract: ExtractContract):
+
+        ret = self.__filter_and_transform_data(extract_contract)
+
+        return ret
+
+    def __filter_and_transform_data(self, extract_contract: ExtractContract) -> List[Dict]:
+        extraction_date = extract_contract.extraction_date
+        data_content = extract_contract.raw_information_content
+
+        transformed_information = []
+
+        for data in data_content:
+            transformed_data = None
+
+            link = data['link']
+
+            if 'artistid=' not in link:
+                continue
+
+            names = None
+
+            if ', ' in data['name']:
+                names = data['name'].split(' ')
+            elif ' ' in data['name']:
+                names = data['name'].split(' ')
+            else:
+                names = [data['name']]
+
+            transformed_data = self.__transform_data(names, link)
+            transformed_data['transformed_date'] = extraction_date
+
+            transformed_information.append(transformed_data)
+
+        ret = transformed_information
+
+        return ret
+
+    def __transform_data(self, names: List, link: str) -> Dict:
+        link_splited = link.split('artistid=')
+        aux = {}
+
+        if len(names) == 2:
+            aux = {
+                "first_name": names[1],
+                "second_name": names[0],
+                "surname": None,
+                "artist_id": link_splited[1],
+                "link": link
+            }
+        elif len(names) == 3:
+            aux = {
+                "first_name": names[2],
+                "second_name": names[0],
+                "surname": names[1],
+                "artist_id": link_splited[1],
+                "link": link
+            }
+        else:
+            aux = {
+                "first_name": names[0],
+                "second_name": None,
+                "surname": None,
+                "artist_id": link_splited[1],
+                "link": link
+            }
+
+        ret = aux
+
+        return ret
